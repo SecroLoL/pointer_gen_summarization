df0d09e0c110faf33935a0663709a980daa2b107
SecroLoL <Alexshanvi@gmail.com>
2024-07-10 23:49:32 -0700

    Update logger statement

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -153,10 +153,9 @@ def calc_running_avg_loss(loss, running_avg_loss, summary_writer, step, decay=0.
     else:
         running_avg_loss = running_avg_loss * decay + (1 - decay) * loss
     running_avg_loss = min(running_avg_loss, 12)  # clip
-    loss_sum = tf.Summary()
-    tag_name = 'running_avg_loss/decay=%f' % (decay)
-    loss_sum.value.add(tag=tag_name, simple_value=running_avg_loss)
-    summary_writer.add_summary(loss_sum, step)
+    
+    with summary_writer.as_default():
+        tf.summary.scalar('running_avg_loss/deacy=%.2f' % decay, running_avg_loss, step=step)
     return running_avg_loss
 
 
