0ab13b8795fd9195a1e8328b5cc7f76004aaf31d
SecroLoL <azshan@stanford.edu>
2024-07-21 22:59:27 -0700

    Change loss to be item() instead of [0]

--- src/eval.py
+++ src/eval.py
@@ -75,7 +75,7 @@ class Evaluate(object):
         batch_avg_loss = sum_step_losses / dec_lens
         loss = torch.mean(batch_avg_loss)
 
-        return loss.data[0]
+        return loss.item()
 
     def run(self):
         start = time.time()
