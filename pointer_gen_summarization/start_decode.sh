80e20e9e9cacf17c606ac7900913389f3a625b59
SecroLoL <azshan@stanford.edu>
2024-09-06 17:29:45 -0700

    Remove unneeded parts

--- pointer_gen_summarization/start_decode.sh
+++
@@ -1,4 +0,0 @@
-export PYTHONPATH=`pwd`
-MODEL=$1
-python3 training_ptr_gen/decode.py $MODEL >& ../log/decode_log &
-
--- pointer_gen_summarization/start_train.sh
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH=`pwd`
-python3 training_ptr_gen/train.py >& ../log/training_log &
-
