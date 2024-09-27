e12d8a9de8ed49f4732b1e499aed006c1c8625c1
SecroLoL <azshan@stanford.edu>
2024-09-26 21:07:14 -0700

    Change ROUGE score from ROUGE-L to ROUGE-LSUM

--- pointer_gen_summarization/rouge_scoring/score_summaries.py
+++ pointer_gen_summarization/rouge_scoring/score_summaries.py
@@ -13,7 +13,7 @@ def read_file(file_path):
         return file.read().strip()
 
 def calculate_rouge_scores(generated_summary, reference_summary):
-    scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
+    scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeLsum'], use_stemmer=True)
     scores = scorer.score(reference_summary, generated_summary)
     return scores
 
