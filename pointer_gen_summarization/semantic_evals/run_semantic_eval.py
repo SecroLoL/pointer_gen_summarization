937e2b4fa57ba1e10d3ee2ff912d7f4e5a97f9b5
SecroLoL <azshan@stanford.edu>
2024-09-30 22:56:23 -0700

    Update script to compute all scores.

--- pointer_gen_summarization/semantic_evals/run_semantic_eval.py
+++ pointer_gen_summarization/semantic_evals/run_semantic_eval.py
@@ -1,17 +1,21 @@
 """
 Runs the semantic eval of decoded model outputs using BERTScore and S-BERT embedding distances.
 """
-from semantic_evals.bertscore import evaluate_directories as evaluate_bertscore
-from semantic_evals.sbert import evaluate_directories as evaluate_sbert_scores
+from semantic_evals.bertscore import evaluate_directories as evaluate_bertscore, average_score as average_bertscore
+from semantic_evals.sbert import evaluate_directories as evaluate_sbert_scores, average_score as average_sbert_scores
 import argparse 
 
 
 def main(gen_dir, ref_dir):
     bert_scores = evaluate_bertscore(gen_dir, ref_dir)
     sbert_scores = evaluate_sbert_scores(gen_dir, ref_dir)
-
-    print(f"BERTScore similarity scores: {bert_scores}")
-    print(f"SBERT similarity scores: {sbert_scores}")
+    
+    bert_avg_score = average_bertscore(bert_scores)
+    sbert_avg_score = average_sbert_scores(sbert_scores)
+    print(f"Average BERTScore similarity score: {bert_avg_score:.4f}")
+    print(f"Average SBERT similarity score: {sbert_avg_score:.4f}")
+    # print(f"BERTScore similarity scores: {bert_scores}")
+    # print(f"SBERT similarity scores: {sbert_scores}")
 
 if __name__ == "__main__":
 
